906b7e5c217306f7b96f8baa81082f909e071741
ktalant <uchkunfan@gmail.com>
2020-02-10 21:37:47 -0500

    updated

--- src/com/lib/ArtemisDeployer.groovy
+++ src/com/lib/ArtemisDeployer.groovy
@@ -1,8 +1,55 @@
 def deployer(){
   node {
-    stage("Stage1"){
-      echo "Hello, World!!!"
-     }
-  }
+    properties([
+            //discard old builds set to 4
+            buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', 
+            daysToKeepStr: '', numToKeepStr: '4')), 
+            
+            parameters([
+                //choice parameter for app version
+                choice(choices: ['version/0.1', 'version/0.2', 'version/0.3', 'version/0.4', 'version/0.5'], 
+                description: 'Choose app version to be deployed', name: 'ARTEMIS_VERSION'), 
+                
+                //choice parameter for app environment
+                choice(choices: ['dev1.talantzon.com', 'qa1.talantzon.com', 'stage1.talantzon.com', 'prod1.talantzon.com'], 
+                description: 'Choose environment ', name: 'ARTEMIS_ENV')])])
+
+
+    stage("Pull repo"){
+        timestamps {
+            ws {
+                checkout([$class: 'GitSCM', branches: [[name: '${ARTEMIS_VERSION}']], 
+                doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], 
+                userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]])
+            }
+        }
+
+    }
+    
+    stage("Install necessary tools"){
+        timestamps {
+            ws {
+                sh '''
+                    ssh centos@${ARTEMIS_ENV} sudo yum install epel-release -y
+                    ssh centos@${ARTEMIS_ENV} sudo yum install python-pip -y
+                    ssh centos@${ARTEMIS_ENV} sudo pip install Flash
+                '''
+            }
+        }
+    }
+
+    stage("Copy Artemis files"){
+        timestamps {
+            ws {
+                sh '''
+                    scp -r * centos@${ARTEMIS_ENV}
+                '''
+            }
+        }
+    }
+    stage("Pull repo"){
+        
+    }
+}
 }
 
